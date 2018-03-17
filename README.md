a1a49c251485cfb9897dd88da21692120c80d055
Jon Wilson <jonwilsoncomedy@gmail.com>
2018-03-16 23:45:33 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wikipedia Flashcard Automator
+Uses Wikipedia's API to automate the process of building flashcard decks from Wikipedia lists and page extracts
---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      wikiExtract: ''
+    };
+    
+  }
+
+  componentDidMount() {
+    fetch("https://en.wikipedia.org/w/api.php?origin=*&action=query&pageids=21490963&prop=extracts&exintro=true&explaintext=true&format=json&formatversion=2")
+      .then(response => response.json())
+      .then(
+        (result) => {
+          console.log(result.query.pages[0].extract);
+          this.setState({
+            wikiExtract: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      ).then(console.log(this.state));
+  }
+  
+  render() {
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Wikipedia Flashcard Automator</h1>
+        </header>
+        <form>
+          Wikipedia page title: <br />
+          <input type="text" name="Page-title" /><br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
