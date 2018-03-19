8d8c612c197d4702d0c26a013850d67b0bc2b4dc
Jon Wilson <jonwilsoncomedy@gmail.com>
2018-03-19 01:45:36 -0500

    adding third fetch step, and ordering the steps and adding comments to explain how these fetches will be used in each step

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super(props);
 
     this.state = {
+      wikiCategories: '',
       wikiList: '',
       wikiExtract: ''
     };
@@ -14,17 +15,46 @@ class App extends Component {
   }
 
   componentDidMount() {
+    // temp values for testing
+    // for step one
+    var listPageTitle = 'List%20of%20Asian%20countries%20by%20GDP';
+    // for step two
+    var categoryTitle = 'Southeast%20Asian%20countries';
+    // for step three
     var pageids = 21490963;
-    var listTitle = 'List%20of%20Presidents%20of%20the%20United%20States';
 
-    fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&pageids=${pageids}&prop=extracts&exintro=true&explaintext=true&format=json&formatversion=2`)
+    // Step one: Input a title for a list page, and use it to find the correct category for the specific list needed
+    fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=${listPageTitle}&prop=categories&cllimit=max&format=json&formatversion=2`)
     .then(response => response.json())
     .then(
       (result) => {
         this.setState({
-          wikiExtract: result.query.pages[0].extract
+          wikiCategories: result.query.pages[0].categories
         });
-        console.log(this.state);
+        console.log("Categories: ", this.state.wikiCategories);
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        console.log(error);
+      }
+    );
+    
+    // Step two: find the correct list using the appropriate category name
+    // Sometimes this may require repeating this step with different 
+    // category names until finding a list that contains the correct results
+    // (i.e. names of Presidents, or Asian countries) listed
+    // by page titles (not Category names), with correlated pageids
+    // Note: Add ability to dynamically choose list limit based on expected number of results
+    fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&list=categorymembers&cmtitle=Category:${categoryTitle}&cmlimit=100&format=json&formatversion=2`)
+    .then(response => response.json())
+    .then(
+      (result) => {
+        this.setState({
+          wikiList: result.query.categorymembers
+        });
+        console.log("List using Category Title: ", this.state.wikiList);
       },
       // Note: it's important to handle errors here
       // instead of a catch() block so that we don't swallow
@@ -34,14 +64,15 @@ class App extends Component {
       }
     );
 
-    fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=${listTitle}&list=categorymembers&cmtitle=Category:Presidents%20of%20the%20United%20States&cmlimit=50&format=json&formatversion=2`)
+    // Step three: Use pageid's to get extracts in plaintext 
+    fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&pageids=${pageids}&prop=extracts&exintro=true&explaintext=true&format=json&formatversion=2`)
     .then(response => response.json())
     .then(
       (result) => {
         this.setState({
-          wikiList: result.query.categorymembers
+          wikiExtract: result.query.pages[0].extract
         });
-        console.log(this.state);
+        console.log("Extract derived from pageid: ", this.state.wikiExtract);
       },
       // Note: it's important to handle errors here
       // instead of a catch() block so that we don't swallow
