735655b08afe172f94d15736ec57bca384796a8b
Jon Wilson <jonwilsoncomedy@gmail.com>
2018-10-07 21:36:30 -0500

    cleaning up

--- src/__tests__/Search.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import SearchButton from '../SearchButton';
-
-test('Search renders correctly', () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
